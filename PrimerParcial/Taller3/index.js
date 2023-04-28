db2a696505e3f54555286dad61fa2481b8fe6bf3
Eliceo-CEDENO <eliceo1139@gmail.com>
2023-04-27 23:51:14 -0500

    agregando màs datos para prueba

--- PrimerParcial/Taller3/index.js
+++ PrimerParcial/Taller3/index.js
@@ -24,15 +24,22 @@ const connecionURL="mongodb+srv://admin1:admin1@cluster0.ldz0ign.mongodb.net/tes
 
         const inversionista1 = new Inversionista({nombre:"Elias Cedeño",identificacion:"1313000000" });
         const inversionista2 = new Inversionista({nombre:"Wilter Zambrano",identificacion:"1314000000"});
+
+        //Agregando más datos
+
+        const inversionista3 = new Inversionista({nombre:"Bruno Zambrano",identificacion:"1315000000"});
         const saveinversionista1 =  await inversionista1.save();
     
         const saveinversionista2 =  await inversionista2.save();
+        const saveinversionista3 = await inversionista3.save();
 
-
+        //Agregando conceptos de inversiòn 
         const conceptosinversion1 = new Conceptosinversion({concepto:"Inversión en índices", detalle:"Diferentes tipos de activos"});
         const conceptosinversion2 = new Conceptosinversion({concepto:"Diversificación", detalle:"Inventir mercados amplios"});
+        const conceptosinversion3 = new Conceptosinversion({concepto:"Análisis fundamental", detalle:"Desarrollo económico en una empresa"});
         const saveconceptosinversion1 =  await conceptosinversion1.save();
         const saveconceptosinversion2 =  await conceptosinversion2.save();
+        const saveconceptosinversion3 =  await conceptosinversion3.save();
 
 
         const inversion1 =  new Inversion({
@@ -44,10 +51,20 @@ const connecionURL="mongodb+srv://admin1:admin1@cluster0.ldz0ign.mongodb.net/tes
 
         })
 
+        const inversion3 =  new Inversion({
+            valorinversion:"4000",
+            fecha:"28/04/2023",
+            duracioninversiondias:"302",
+            idinversionista: saveinversionista3._id,
+            idconceptoinversion:saveconceptosinversion3._id 
+
+        })
+
 
 
 
         const saveInversion =  await inversion1.save();
+        const saveInversion2 =  await inversion3.save();
         const result=  await Inversion.find().populate(["idinversionista","idconceptoinversion"])
        // console.log(result  );
         
